7144b0c4613202a4f5851b45ca6cdadc06e756f5
NAKAYAMA Atsushi <a-nakayama@vc-robotics.com>
2020-02-17 12:38:16 +0900

    use submodule

--- linux/build.sh
+++ linux/build.sh
@@ -8,7 +8,7 @@ cmake \
   -DCMAKE_INSTALL_PREFIX=/opt/vsm-sdk/linux \
   -DUGCS_INSTALL_DIR=/opt/vsm-sdk/linux \
   -DVSM_SDK_DIR=/opt/vsm-sdk/linux/opt/vsm-sdk/ \
-  -DCOMMON_SOURCES="../vsm-cpp-common" \
+  -DCOMMON_SOURCES="./vsm-cpp-common" \
   -DCMAKE_CXX_FLAGS="-Wno-error=implicit-fallthrough -Wno-error=old-style-cast -Wno-error=unused-parameter -g3" \
   -G"Unix Makefiles" \
   -DCMAKE_BUILD_TYPE=Release \
